a6dd474f26c2089e0f8d00ca251f7fb14b9a4082
hohlayder <106448703+hohlayder@users.noreply.github.com>
2024-09-08 16:18:31 +0300

    Add files via upload

--- func4.py
+++ func4.py
@@ -1,6 +1,6 @@
 def describe_person(name, age=30):
-    print(f'Имя: {name}; Возраст: {age} '
-          f'{'лет' if age % 10 >= 5 or age % 10 == 0 or age // 10 % 10 == 1 else ('год' if age % 10 == 1 else 'года')}')
+    print(
+        f'Имя: {name}; Возраст: {age} {"лет" if age % 10 >= 5 or age % 10 == 0 or age // 10 % 10 == 1 else ("год" if age % 10 == 1 else "года")}')
 
 
 describe_person("Женя")
